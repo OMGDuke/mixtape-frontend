24ae358080b1fc85a841bf90b5506ab48d59d446
OMGDuke <alex@omgduke.com>
2018-07-08 23:26:43 +0900

    Changed the way firebase config is handled for netlify

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,21 +7,27 @@
 
 <script>
 import firebase from 'firebase';
-import firebaseConfig from '../assets/firebase-config.json';
 
 export default {
   name: 'dashboard',
   created() {
     const { firebaseToken } = this.$route.query;
-    const config = firebaseConfig;
+    const config = {
+      apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+      authDomain: `${process.env.VUE_APP_FIREBASE_APP}.firebaseapp.com`,
+      databaseURL: `https://${process.env.VUE_APP_FIREBASE_APP}.firebaseio.com`,
+      projectId: process.env.VUE_APP_FIREBASE_APP,
+      storageBucket: `${process.env.VUE_APP_FIREBASE_APP}.appspot.com`,
+      messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING
+    };
     firebase.initializeApp(config);
     firebase
       .auth()
       .signInWithCustomToken(firebaseToken)
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
       });
-  },
+  }
 };
 </script>
 
